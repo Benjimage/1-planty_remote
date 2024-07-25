ee1598ea8d6739b414f4f89fde517d8db1967881
benjamin <benjbruant@gmail.com>
2024-07-25 16:40:45 +0200

    création des fichiers de structure du theme

---
+++ wp-content/themes/Planty/footer.php
@@ -0,0 +1,11 @@
+</main>
+</div>
+<footer id="footer" role="contentinfo">
+<div>
+<a href="https://localhost/1-home/ProjetOC/3-P6/Planty/mentions-legales" target="_blank" class="mentions-legales">Mentions Légales</a>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
