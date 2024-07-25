489d11994a4abf2193cf8d83e131114c2e82addc
benjamin <benjbruant@gmail.com>
2024-07-25 16:35:27 +0200

    installation du theme enfant

---
+++ wp-content/themes/Planty/functions.php
@@ -0,0 +1,18 @@
+<?php 
+add_action( 'wp_enqueue_scripts', 'planty_enqueue_styles' ); 
+
+	function planty_enqueue_styles() { 
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+	wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css')); 
+
+	}
+
+add_filter('wp_nav_menu_items','add_admin_link',10,2);
+
+function add_admin_link($items, $args) {
+	if (is_user_logged_in() &&$args->theme_location === 'main-menu') {
+		$items.= '<li class="lien-admin"><a href="' . get_admin_url() .'">Admin</a></li>';
+
+	}
+	return $items;
+}
